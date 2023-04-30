a9e378b4f2de256513d6638c38f8e664d42c1c8c
BernardoSch <132226687+BernardoSch@users.noreply.github.com>
2023-04-30 18:20:19 -0300

    v

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,8 @@ language = 'pt_BR'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'furo'
+#html_theme = 'sphinx_book_theme'
 #html_theme = 'karma_sphinx_theme'
 #html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
